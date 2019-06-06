c2e7700670beec0e09623909d44985724f5519b8
hxdegawa <hxdegawa@gmail.com>
2019-06-06 14:33:51 +0900

    Implemented Rapid check system

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const ping = require('ping');
 const cron = require('node-cron');
 
-cron.schedule('*/5 * * * * *', () => {
-  ping.sys.probe("8.8.8.8", function(isAlive){
+cron.schedule('*/1 * * * * *', () => {
+  ping.sys.probe("8.8.8.8", function(isAlive) {
     const msg = isAlive ? 'host ' + "8.8.8.8" + ' is alive' : 'host ' + "8.8.8.8" + ' is dead';
     console.log(msg);
+    if (!isAlive) {
+      const rapidCheck = cron.schedule("*/0.1 * * * * *", () => {
+        ping.sys.probe("8.8.8.8", function(liveChecked) {
+          console.log(liveChecked);
+        });
+      });
+
+      setTImeout(() => {
+        rapidCheck.destroy();
+      }, 1000)
+    };
   });
 });
